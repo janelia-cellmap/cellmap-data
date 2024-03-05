22206e8d486eb57b0699a88163d3f83639b6658a
rhoadesScholar <rhoadesj@hhmi.org>
2024-03-05 12:43:09 -0500

    Add dependencies to pyproject.toml and set default version to 0.0.0 in __init__.py

--- src/cellmap_data/__init__.py
+++ src/cellmap_data/__init__.py
@@ -1,10 +1,16 @@
-"""Utility for loading CellMap data for machine learning training, utilizing PyTorch, TensorStore, and PyDantic."""
+"""
+CellMap Data Loading Module.
+
+Utility for loading CellMap data for machine learning training,
+utilizing PyTorch, TensorStore, and PyDantic.
+"""
+
 from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("cellmap.data")
 except PackageNotFoundError:
-    __version__ = "uninstalled"
+    __version__ = "0.0.0"
 
 __author__ = "Jeff Rhoades"
 __email__ = "rhoadesj@hhmi.org"
