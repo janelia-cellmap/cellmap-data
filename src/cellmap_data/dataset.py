fix: 🚀 Change class weighting strategy

src/cellmap_data/dataset.py
@@ -376,54 +376,57 @@ class CellMapDataset(Dataset):
 
     @property
     def class_counts(self) -> Dict[str, Dict[str, float]]:
-        """Returns the number of pixels for each class in the ground truth data, normalized by the resolution."""
+        """
+        Returns the number of pixels for each class in the ground truth data, normalized by the resolution.
+        """
         if not hasattr(self, "_class_counts"):
             class_counts = {"totals": {c: 0.0 for c in self.classes}}
             class_counts["totals"]["bg"] = 0.0
             for array_name, sources in self.target_sources.items():
                 class_counts[array_name] = {}
-                bg_count = -1.0
+                bg_count = None
                 for label, source in sources.items():
                     if isinstance(source, Sequence):
-                        class_counts[array_name][label] = 0
-                        # class_counts["totals"][label] += 0
+                        class_counts[array_name][label] = 0.0
                     else:
-                        if bg_count < 0.0:
+                        if bg_count is None:
                             bg_count = np.prod(
-                                list(source.output_size.values()), dtype=float
+                                [
+                                    stop - start
+                                    for start, stop in self.bounding_box.values()
+                                ]
                             )
                         bg_count -= source.class_counts
                         class_counts[array_name][label] = source.class_counts
                         class_counts["totals"][label] += source.class_counts
-                class_counts[array_name]["bg"] = bg_count
-                class_counts["totals"]["bg"] += bg_count
+                if bg_count is None or bg_count < 0:
+                    # TODO: If there are overlapping classes, the background count may be negative. This needs a better solution.
+                    bg_count = 0
+                class_counts[array_name]["bg"] = bg_count  # type: ignore
+                class_counts["totals"]["bg"] += bg_count  # type: ignore
             self._class_counts = class_counts
         return self._class_counts
 
     @property
     def class_weights(self) -> dict[str, float]:
         """
-        Returns the class weights for the dataset based on the number of samples in each class.
+        Returns the class weights for the dataset based on the number of samples in each class. Classes without any samples will have a weight of NaN.
         """
         if not hasattr(self, "_class_weights"):
-            if len(self.classes) > 1:
-                class_count_sum = 0
-                for c in self.classes:
-                    class_count_sum += self.class_counts["totals"][c]
-                class_count_sum += self.class_counts["totals"]["bg"]
+            # TODO: Review this implementation - not weighting compared to background class seems wrong
+            class_count_sum = 0
+            for c in self.classes:
+                class_count_sum += self.class_counts["totals"][c]
+            # class_count_sum += self.class_counts["totals"]["bg"]
 
-                class_weights = {
-                    c: (
-                        # CLASS_WEIGHTS SHOULD BE > 1 FOR FOREGROUND CLASSES - I THINK THIS NEEDS TO BE THE INVERSE, NOT 1 - ...
-                        # BUT THAT WILL BE PRETTY BIG...
-                        1 - (self.class_counts["totals"][c] / class_count_sum)
-                        if self.class_counts["totals"][c] != class_count_sum
-                        else 0.1
-                    )
-                    for c in self.classes
-                }
-            else:
-                class_weights = {self.classes[0]: 0.1}  # less than 1 to avoid overflow
+            class_weights = {
+                c: (
+                    class_count_sum / self.class_counts["totals"][c]
+                    if self.class_counts["totals"][c] != 0
+                    else np.NaN
+                )
+                for c in self.classes
+            }
             self._class_weights = class_weights
         return self._class_weights
 

src/cellmap_data/image.py
@@ -107,11 +107,21 @@ class CellMapImage:
         return f"CellMapImage({self.path})"
 
     @property
+    def scale_level(self) -> str:
+        """Returns the multiscale level of the image."""
+        if not hasattr(self, "_scale_level"):
+            self._scale_level = self.find_level(self.scale)
+        return self._scale_level
+
+    @property
+    def group(self) -> xarray.DataArray:
+        if not hasattr(self, "_group"):
+            self._group = read_xarray(self.path)
+        return self._group  # type: ignore
+
+    @property
     def array(self) -> xarray.DataArray:
         if not hasattr(self, "_array"):
-            self.group = read_xarray(self.path)
-            # Find correct multiscale level based on target scale
-            self.scale_level = self.find_level(self.scale)
             self.array_path = os.path.join(self.path, self.scale_level)
             # Construct an xarray with Tensorstore backend
             ds = read_xarray(self.array_path)
@@ -315,7 +325,8 @@ class EmptyImage:
         }
         self.axes = axis_order
         self._bounding_box = None
-        self._class_counts = 0
+        self._class_counts = 0.0
+        self._bg_count = 0.0
         self.scale = {c: sc for c, sc in zip(self.axes, self.target_scale)}
         self.empty_value = empty_value
         if store is not None:
@@ -343,9 +354,7 @@ class EmptyImage:
     @property
     def bg_count(self) -> float:
         """Returns the number of background pixels in the ground truth data, normalized by the resolution."""
-        return np.prod(
-            [s * sc for s, sc in zip(self.output_shape.values(), self.scale.values())]
-        ).astype(int)
+        return self._bg_count
 
     @property
     def class_counts(self) -> float:

src/cellmap_data/multidataset.py
@@ -37,14 +37,13 @@ class CellMapMultiDataset(ConcatDataset):
         return out_string
 
     @property
-    def class_counts(self):
-        if (
-            not hasattr(self, "_class_counts")
-            or self._class_counts is None  # This should be overkill...
-            or len(self._class_counts) == 0
-        ):
+    def class_counts(self) -> dict[str, dict[str, float]]:
+        """
+        Returns the number of samples in each class for each dataset in the multi-dataset, as well as the total number of samples in each class.
+        """
+        if not hasattr(self, "_class_counts"):
             class_counts = {}
-            for c in self.classes:
+            for c in list(self.classes) + ["bg"]:
                 class_counts[c] = {}
                 total = 0.0
                 for ds in self.datasets:
@@ -59,24 +58,24 @@ class CellMapMultiDataset(ConcatDataset):
         """
         Returns the class weights for the multi-dataset based on the number of samples in each class.
         """
+        # TODO: review this implementation
         if not hasattr(self, "_class_weights"):
-            if len(self.classes) > 1:
-                class_counts = {}
-                class_count_sum = 0
-                for c in self.classes:
-                    class_counts[c] = self.class_counts[c]["total"]
-                    class_count_sum += class_counts[c]
+            # TODO: Review this implementation - not weighting compared to background class seems wrong
+            class_counts = {}
+            class_count_sum = 0
+            for c in self.classes:
+                class_counts[c] = self.class_counts[c]["total"]
+                class_count_sum += class_counts[c]
+            # class_count_sum += self.class_counts["bg"]["total"]
 
-                class_weights = {
-                    c: (
-                        1 - (class_counts[c] / class_count_sum)
-                        if class_counts[c] != class_count_sum
-                        else 0.1
-                    )
-                    for c in self.classes
-                }
-            else:
-                class_weights = {self.classes[0]: 0.1}  # less than 1 to avoid overflow
+            class_weights = {
+                c: (
+                    class_count_sum / class_counts[c]
+                    if class_counts[c] != 0
+                    else np.NaN
+                )
+                for c in self.classes
+            }
             self._class_weights = class_weights
         return self._class_weights
 
@@ -86,13 +85,11 @@ class CellMapMultiDataset(ConcatDataset):
         Returns the weights for each dataset in the multi-dataset based on the number of samples in each dataset.
         """
         if not hasattr(self, "_dataset_weights"):
-            class_weights = self.class_weights
-
             dataset_weights = {}
             for dataset in self.datasets:
                 dataset_weight = np.sum(
                     [
-                        dataset.class_counts["totals"][c] * class_weights[c]
+                        dataset.class_counts["totals"][c] * self.class_weights[c]
                         for c in self.classes
                     ]
                 )
@@ -162,7 +159,7 @@ class CellMapMultiDataset(ConcatDataset):
                 generator=rng,
             )
         else:
-            dataset_weights = list(self.dataset_weights.values())
+            dataset_weights = [self.dataset_weights[ds] for ds in self.datasets]
 
             datasets_sampled = torch.multinomial(
                 torch.tensor(dataset_weights), num_samples, replacement=True
@@ -171,8 +168,11 @@ class CellMapMultiDataset(ConcatDataset):
             index_offset = 0
             for i, dataset in enumerate(self.datasets):
                 if len(dataset) == 0:
+                    RuntimeWarning(f"Dataset {dataset} has no samples, skipping")
                     continue
                 count = (datasets_sampled == i).sum().item()
+                if count == 0:
+                    continue
                 dataset_indices = torch.randint(
                     0, len(dataset) - 1, [count], generator=rng
                 )