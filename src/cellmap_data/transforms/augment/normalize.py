c62611affa056beeb1f2f3ee45f19a5c3ecbc24b
rhoadesScholar <rhoadesj@hhmi.org>
2024-07-22 13:04:01 -0400

    docs: 📝 Update transforms/augment/normalize.py docs.

--- src/cellmap_data/transforms/augment/normalize.py
+++ src/cellmap_data/transforms/augment/normalize.py
@@ -3,10 +3,18 @@ import torchvision.transforms.v2 as T
 
 
 class Normalize(T.Transform):
+    """Normalize the input tensor. Subclasses torchvision.transforms.Transform.
+
+    Methods:
+        _transform: Transform the input.
+    """
+
     def __init__(self):
+        """Initialize the normalization transformation."""
         super().__init__()
 
     def _transform(self, x: Any, params: Dict[str, Any]) -> Any:
+        """Transform the input."""
         min_val = x.nan_to_num().min()
         diff = x.nan_to_num().max() - min_val
         if diff == 0:
