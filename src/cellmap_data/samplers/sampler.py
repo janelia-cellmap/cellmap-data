ff2813e04c25e1b5ffba872ddaeee7f12bf3ebb0
rhoadesScholar <rhoadesj@hhmi.org>
2024-07-22 13:48:28 -0400

    docs: 📝 Update samplers/sampler.py docs.

--- src/cellmap_data/samplers/sampler.py
+++ src/cellmap_data/samplers/sampler.py
@@ -4,6 +4,19 @@ from torch.utils.data import Sampler
 
 
 class CellMapSampler(Sampler):
+    """CellMap sampler.
+
+    Attributes:
+        data_source (Sized): Data source.
+        indices (Sequence[int]): Indices.
+        check_function (Optional[Callable]): Check function.
+        generator (Optional[torch.Generator]): Generator.
+
+    Methods:
+        __iter__: Iterate over the indices.
+        __len__: Return the length of the indices
+    """
+
     def __init__(
         self,
         data_source: Sized,
@@ -11,6 +24,14 @@ class CellMapSampler(Sampler):
         check_function: Optional[Callable] = None,
         generator: Optional[torch.Generator] = None,
     ):
+        """Initialize the CellMap sampler.
+
+        Args:
+            data_source (Sized): Data source.
+            indices (Sequence[int]): Indices.
+            check_function (Optional[Callable], optional): Check function. Defaults to None.
+            generator (Optional[torch.Generator], optional): Generator. Defaults to None.
+        """
         super().__init__()
         self.data_source = data_source
         self.indices = indices
@@ -18,9 +39,15 @@ class CellMapSampler(Sampler):
         self.generator = generator
 
     def __iter__(self):
+        """Iterate over the indices
+
+        Yields:
+            int: Index
+        """
         for i in torch.randperm(len(self.indices), generator=self.generator):
             if self.check_function is None or self.check_function(self.indices[i]):
                 yield self.indices[i]
 
     def __len__(self):
+        """Return the number of indices"""
         return len(self.indices)
