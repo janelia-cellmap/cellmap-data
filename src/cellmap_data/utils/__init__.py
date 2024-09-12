efba52c83b2a31f71ca6f67a9201a7ea7dabc076
rhoadesScholar <rhoadesj@hhmi.org>
2024-09-12 10:22:16 -0400

    fix: 🐛 Fix __init__ import

--- src/cellmap_data/utils/__init__.py
+++ src/cellmap_data/utils/__init__.py
@@ -1,2 +1,2 @@
-from .figs import get_image_grid, get_image_dict, get_image_grid_from_dict
+from .figs import get_image_grid, get_image_dict, get_image_grid_numpy
 from .dtype import torch_max_value
