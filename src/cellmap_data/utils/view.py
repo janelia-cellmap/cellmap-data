d490a24fc1d393cd4e46f1fae90fde70f899f145
rhoadesScholar <rhoadesj@hhmi.org>
2026-02-18 15:09:29 -0500

    feat: refactor neuroglancer integration and improve scale pyramid handling

--- src/cellmap_data/utils/view.py
+++ src/cellmap_data/utils/view.py
@@ -8,15 +8,8 @@ import urllib.parse
 import webbrowser
 from multiprocessing.pool import ThreadPool
 
-import neuroglancer
 import numpy as np
-import s3fs
 import zarr
-from IPython.core.getipython import get_ipython
-from IPython.display import IFrame, display
-from tensorstore import d as ts_d
-from tensorstore import open as ts_open
-from upath import UPath
 
 logger = logging.getLogger(__name__)
 
@@ -30,6 +23,8 @@ S3_RAW_NAME = "em/fibsem-uint8"
 
 def get_multiscale_voxel_sizes(path: str):
     if "s3://" in path:
+        import s3fs
+
         # Use s3fs to read the zarr metadata
         fs = s3fs.S3FileSystem(anon=True)
         store = s3fs.S3Map(
@@ -114,6 +109,10 @@ def open_neuroglancer(metadata):
 
     Returns the Neuroglancer.Viewer object.
     """
+    import neuroglancer
+    from IPython.core.getipython import get_ipython
+    from IPython.display import IFrame, display
+
     # 1) bind to localhost on a random port
     neuroglancer.set_server_bind_address("localhost", 0)
     viewer = neuroglancer.Viewer()
@@ -178,7 +177,7 @@ def get_layer(
     data_path: str,
     layer_type: str = "image",
     multiscale: bool = True,
-) -> neuroglancer.Layer:
+):
     """
     Get a Neuroglancer layer from a zarr data path for a LocalVolume.
 
@@ -196,6 +195,9 @@ def get_layer(
     neuroglancer.Layer
         The Neuroglancer layer.
     """
+    import neuroglancer
+    from upath import UPath
+
     # Construct an xarray with Tensorstore backend
     # Get metadata
     if multiscale:
@@ -217,6 +219,115 @@ def get_layer(
                     voxel_offset=metadata[scale]["voxel_offset"],
                 )
             )
+        class ScalePyramid(neuroglancer.LocalVolume):
+            """A neuroglancer layer that provides volume data on different scales.
+            Mimics a LocalVolume.
+            From https://github.com/funkelab/funlib.show.neuroglancer/blob/master/funlib/show/neuroglancer/scale_pyramid.py
+
+            Args:
+            ----
+                    volume_layers (``list`` of ``LocalVolume``):
+
+                        One ``LocalVolume`` per provided resolution.
+            """
+
+            def __init__(self, volume_layers):
+                volume_layers = volume_layers
+
+                super(neuroglancer.LocalVolume, self).__init__()
+
+                logger.debug("Creating scale pyramid...")
+
+                self.min_voxel_size = min(
+                    [tuple(layer.dimensions.scales) for layer in volume_layers]
+                )
+                self.max_voxel_size = max(
+                    [tuple(layer.dimensions.scales) for layer in volume_layers]
+                )
+
+                self.dims = len(volume_layers[0].dimensions.scales)
+                self.volume_layers = {
+                    tuple(
+                        int(x)
+                        for x in map(
+                            operator.truediv,
+                            layer.dimensions.scales,
+                            self.min_voxel_size,
+                        )
+                    ): layer
+                    for layer in volume_layers
+                }
+
+                logger.debug("min_voxel_size: %s", self.min_voxel_size)
+                logger.debug("scale keys: %s", self.volume_layers.keys())
+                logger.debug(self.info())
+
+            @property
+            def volume_type(self):
+                return self.volume_layers[(1,) * self.dims].volume_type
+
+            @property
+            def token(self):
+                return self.volume_layers[(1,) * self.dims].token
+
+            def info(self):
+                reference_layer = self.volume_layers[(1,) * self.dims]
+                reference_info = reference_layer.info()
+
+                info = {
+                    "dataType": reference_info["dataType"],
+                    "encoding": reference_info["encoding"],
+                    "generation": reference_info["generation"],
+                    "coordinateSpace": reference_info["coordinateSpace"],
+                    "shape": reference_info["shape"],
+                    "volumeType": reference_info["volumeType"],
+                    "voxelOffset": reference_info["voxelOffset"],
+                    "chunkLayout": reference_info["chunkLayout"],
+                    "downsamplingLayout": reference_info["downsamplingLayout"],
+                    "maxDownsampling": int(
+                        np.prod(
+                            np.array(self.max_voxel_size)
+                            // np.array(self.min_voxel_size)
+                        )
+                    ),
+                    "maxDownsampledSize": reference_info["maxDownsampledSize"],
+                    "maxDownsamplingScales": reference_info["maxDownsamplingScales"],
+                }
+
+                return info
+
+            def get_encoded_subvolume(self, data_format, start, end, scale_key=None):
+                if scale_key is None:
+                    scale_key = ",".join(("1",) * self.dims)
+
+                scale = tuple(int(s) for s in scale_key.split(","))
+                closest_scale = None
+                min_diff = np.inf
+                for volume_scales in self.volume_layers.keys():
+                    scale_diff = np.array(scale) // np.array(volume_scales)
+                    if any(scale_diff < 1):
+                        continue
+                    scale_diff = scale_diff.max()
+                    if scale_diff < min_diff:
+                        min_diff = scale_diff
+                        closest_scale = volume_scales
+
+                assert closest_scale is not None
+                relative_scale = np.array(scale) // np.array(closest_scale)
+
+                return self.volume_layers[closest_scale].get_encoded_subvolume(
+                    data_format,
+                    start,
+                    end,
+                    scale_key=",".join(map(str, relative_scale)),
+                )
+
+            def get_object_mesh(self, object_id):
+                return self.volume_layers[(1,) * self.dims].get_object_mesh(object_id)
+
+            def invalidate(self):
+                return self.volume_layers[(1,) * self.dims].invalidate()
+
         volume = ScalePyramid(layers)
 
     else:
@@ -319,114 +430,10 @@ def parse_multiscale_metadata(data_path: str):
     return scales, parsed
 
 
-class ScalePyramid(neuroglancer.LocalVolume):
-    """A neuroglancer layer that provides volume data on different scales.
-    Mimics a LocalVolume.
-    From https://github.com/funkelab/funlib.show.neuroglancer/blob/master/funlib/show/neuroglancer/scale_pyramid.py
-
-    Args:
-    ----
-            volume_layers (``list`` of ``LocalVolume``):
-
-                One ``LocalVolume`` per provided resolution.
-    """
-
-    def __init__(self, volume_layers):
-        volume_layers = volume_layers
-
-        super(neuroglancer.LocalVolume, self).__init__()
-
-        logger.debug("Creating scale pyramid...")
-
-        self.min_voxel_size = min(
-            [tuple(layer.dimensions.scales) for layer in volume_layers]
-        )
-        self.max_voxel_size = max(
-            [tuple(layer.dimensions.scales) for layer in volume_layers]
-        )
-
-        self.dims = len(volume_layers[0].dimensions.scales)
-        self.volume_layers = {
-            tuple(
-                int(x)
-                for x in map(
-                    operator.truediv, layer.dimensions.scales, self.min_voxel_size
-                )
-            ): layer
-            for layer in volume_layers
-        }
-
-        logger.debug("min_voxel_size: %s", self.min_voxel_size)
-        logger.debug("scale keys: %s", self.volume_layers.keys())
-        logger.debug(self.info())
-
-    @property
-    def volume_type(self):
-        return self.volume_layers[(1,) * self.dims].volume_type
-
-    @property
-    def token(self):
-        return self.volume_layers[(1,) * self.dims].token
-
-    def info(self):
-        reference_layer = self.volume_layers[(1,) * self.dims]
-        # return reference_layer.info()
-
-        reference_info = reference_layer.info()
-
-        info = {
-            "dataType": reference_info["dataType"],
-            "encoding": reference_info["encoding"],
-            "generation": reference_info["generation"],
-            "coordinateSpace": reference_info["coordinateSpace"],
-            "shape": reference_info["shape"],
-            "volumeType": reference_info["volumeType"],
-            "voxelOffset": reference_info["voxelOffset"],
-            "chunkLayout": reference_info["chunkLayout"],
-            "downsamplingLayout": reference_info["downsamplingLayout"],
-            "maxDownsampling": int(
-                np.prod(np.array(self.max_voxel_size) // np.array(self.min_voxel_size))
-            ),
-            "maxDownsampledSize": reference_info["maxDownsampledSize"],
-            "maxDownsamplingScales": reference_info["maxDownsamplingScales"],
-        }
-
-        return info
-
-    def get_encoded_subvolume(self, data_format, start, end, scale_key=None):
-        if scale_key is None:
-            scale_key = ",".join(("1",) * self.dims)
-
-        scale = tuple(int(s) for s in scale_key.split(","))
-        closest_scale = None
-        min_diff = np.inf
-        for volume_scales in self.volume_layers.keys():
-            scale_diff = np.array(scale) // np.array(volume_scales)
-            if any(scale_diff < 1):
-                continue
-            scale_diff = scale_diff.max()
-            if scale_diff < min_diff:
-                min_diff = scale_diff
-                closest_scale = volume_scales
-
-        assert closest_scale is not None
-        relative_scale = np.array(scale) // np.array(closest_scale)
-
-        return self.volume_layers[closest_scale].get_encoded_subvolume(
-            data_format,
-            start,
-            end,
-            scale_key=",".join(map(str, relative_scale)),
-        )
-
-    def get_object_mesh(self, object_id):
-        return self.volume_layers[(1,) * self.dims].get_object_mesh(object_id)
-
-    def invalidate(self):
-        return self.volume_layers[(1,) * self.dims].invalidate()
-
-
 def open_ds_tensorstore(dataset_path: str, mode="r", concurrency_limit=None):
+    from tensorstore import d as ts_d
+    from tensorstore import open as ts_open
+
     # open with zarr or n5 depending on extension
     filetype = (
         "zarr" if dataset_path.rfind(".zarr") > dataset_path.rfind(".n5") else "n5"
